871f78a38cc98abc42400492a5180898b871fd2c
Jake Luecke <jrluecke95@gmail.com>
2021-02-15 12:50:19 -0500

    weather app acutally functions

--- weatherApp.js
+++ weatherApp.js
@@ -14,11 +14,19 @@
 $(document).ready(function () {
     const $button = $('#weatherButton');
     const $zipcode = $('#zipcode')
-    const $temp = $('#temp')
+    const $tempInfo = $('#temp')
+    const $feelsLikeInfo = $('#feelsLike')
     $button.on('click', (e) => {
         e.preventDefault()
-        const weatherInfo = $.getJSON(`http://api.openweathermap.org/data/2.5/weather?&units=imperial&zip=${$zipcode.val()},us&appid=16b98922cbf6a3314343414c043da48e`)
-        console.log(JSON.parse(weatherInfo))
+        $.getJSON(`http://api.openweathermap.org/data/2.5/weather?&units=imperial&zip=${$zipcode.val()},us&appid=16b98922cbf6a3314343414c043da48e`)
+        .then((data) => {
+            const weatherInfo = data;
+            const temp = weatherInfo.main.temp
+            const feelsLike = weatherInfo.main.feels_like
+        $tempInfo.html(`Your current temperature is ${temp}`)
+        $feelsLikeInfo.html(`It feels like ${feelsLike}`)
+        
+        })
     })
 
 
